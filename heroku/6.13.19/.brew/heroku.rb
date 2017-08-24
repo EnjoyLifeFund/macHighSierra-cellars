6b92f29c4b06131ae3596b3a225bb17a98f6d608
ralic <raliclo@gmail.com>
2017-08-24 16:12:54 -0700

    Update packages

---
+++ heroku/6.13.19/.brew/heroku.rb
@@ -0,0 +1,23 @@
+require "language/node"
+
+class Heroku < Formula
+  desc "Command-line client for the cloud PaaS"
+  homepage "https://cli.heroku.com"
+  url "https://registry.npmjs.org/heroku-cli/-/heroku-cli-6.13.19.tgz"
+  sha256 "c46e8c4f44126ba1f9a3ea19439bd75bc945aa52cd5b6cf8b34759a333fb70cf"
+  head "https://github.com/heroku/cli.git"
+
+  depends_on "node"
+
+  def install
+    inreplace "bin/run.js", "npm update -g heroku-cli", "brew upgrade heroku"
+    inreplace "bin/run", "node \"$DIR/run.js\"",
+                         "#{Formula["node"].opt_bin}/node \"$DIR/run.js\""
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"heroku", "version"
+  end
+end
