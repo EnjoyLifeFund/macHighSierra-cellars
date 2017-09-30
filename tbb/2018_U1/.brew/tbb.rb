4aa3156127fa2815c149ebd21b2cbdbfa9088940
ralic <Raliclo@gmail.com>
2017-09-30 16:03:43 +0800

    Add libs

---
+++ tbb/2018_U1/.brew/tbb.rb
@@ -0,0 +1,49 @@
+class Tbb < Formula
+  desc "Rich and complete approach to parallelism in C++"
+  homepage "https://www.threadingbuildingblocks.org/"
+  url "https://github.com/01org/tbb/archive/2018_U1.tar.gz"
+  version "2018_U1"
+  sha256 "c6462217d4ecef2b44fce63cfdf31f9db4f6ff493869899d497a5aef68b05fc5"
+
+  option :cxx11
+
+  # requires malloc features first introduced in Lion
+  # https://github.com/Homebrew/homebrew/issues/32274
+  depends_on :macos => :lion
+  depends_on :python if MacOS.version <= :snow_leopard
+  depends_on "swig" => :build
+
+  def install
+    compiler = (ENV.compiler == :clang) ? "clang" : "gcc"
+    args = %W[tbb_build_prefix=BUILDPREFIX compiler=#{compiler}]
+
+    if build.cxx11?
+      ENV.cxx11
+      args << "cpp0x=1" << "stdlib=libc++"
+    end
+
+    system "make", *args
+    lib.install Dir["build/BUILDPREFIX_release/*.dylib"]
+    include.install "include/tbb"
+
+    cd "python" do
+      ENV["TBBROOT"] = prefix
+      system "python", *Language::Python.setup_install_args(prefix)
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <tbb/task_scheduler_init.h>
+      #include <iostream>
+
+      int main()
+      {
+        std::cout << tbb::task_scheduler_init::default_num_threads();
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-L#{lib}", "-ltbb", "-o", "test"
+    system "./test"
+  end
+end
