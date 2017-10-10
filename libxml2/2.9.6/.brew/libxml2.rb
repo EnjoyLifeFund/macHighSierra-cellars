49a477d42f081e52f4c5bdd39535156a2df52d09
ralic <Raliclo@gmail.com>
2017-10-10 21:00:08 +0800

    Update

---
+++ libxml2/2.9.6/.brew/libxml2.rb
@@ -0,0 +1,57 @@
+class Libxml2 < Formula
+  desc "GNOME XML library"
+  homepage "http://xmlsoft.org"
+  url "http://xmlsoft.org/sources/libxml2-2.9.6.tar.gz"
+  mirror "ftp://xmlsoft.org/libxml2/libxml2-2.9.6.tar.gz"
+  sha256 "8b9038cca7240e881d462ea391882092dfdc6d4f483f72683e817be08df5ebbc"
+  head "https://git.gnome.org/browse/libxml2.git"
+
+  keg_only :provided_by_macos
+
+  depends_on :python if MacOS.version <= :snow_leopard
+
+  # These should return to being head-only whenever 2.9.5 is released.
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+
+  def install
+    system "autoreconf", "-fiv"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--without-python",
+                          "--without-lzma"
+    system "make"
+    ENV.deparallelize
+    system "make", "install"
+
+    cd "python" do
+      # We need to insert our include dir first
+      inreplace "setup.py", "includes_dir = [", "includes_dir = ['#{include}', '#{MacOS.sdk_path}/usr/include',"
+      system "python", "setup.py", "install", "--prefix=#{prefix}"
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <libxml/tree.h>
+
+      int main()
+      {
+        xmlDocPtr doc = xmlNewDoc(BAD_CAST "1.0");
+        xmlNodePtr root_node = xmlNewNode(NULL, BAD_CAST "root");
+        xmlDocSetRootElement(doc, root_node);
+        xmlFreeDoc(doc);
+        return 0;
+      }
+    EOS
+    args = shell_output("#{bin}/xml2-config --cflags --libs").split
+    args += %w[test.c -o test]
+    system ENV.cc, *args
+    system "./test"
+
+    ENV.prepend_path "PYTHONPATH", lib/"python2.7/site-packages"
+    system "python", "-c", "import libxml2"
+  end
+end
---
+++ pyqt/5.9_1/.brew/pyqt.rb
@@ -0,0 +1,64 @@
+class Pyqt < Formula
+  desc "Python bindings for v5 of Qt"
+  homepage "https://www.riverbankcomputing.com/software/pyqt/download5"
+  url "https://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.9/PyQt5_gpl-5.9.tar.gz"
+  sha256 "ab0e7999cf202cc72962c78aefe461d16497b3c1a8282ab966ad90b6cb271096"
+  revision 1
+
+  option "with-debug", "Build with debug symbols"
+  option "with-docs", "Install HTML documentation and python examples"
+
+  deprecated_option "enable-debug" => "with-debug"
+
+  depends_on "qt"
+  depends_on "sip"
+  depends_on :python => :recommended
+  depends_on :python3 => :recommended
+
+  def install
+    if build.without?("python3") && build.without?("python")
+      odie "pyqt: --with-python3 must be specified when using --without-python"
+    end
+
+    Language::Python.each_python(build) do |python, version|
+      args = ["--confirm-license",
+              "--bindir=#{bin}",
+              "--destdir=#{lib}/python#{version}/site-packages",
+              "--stubsdir=#{lib}/python#{version}/site-packages/PyQt5",
+              "--sipdir=#{share}/sip/Qt5",
+              # sip.h could not be found automatically
+              "--sip-incdir=#{Formula["sip"].opt_include}",
+              "--qmake=#{Formula["qt"].bin}/qmake",
+              # Force deployment target to avoid libc++ issues
+              "QMAKE_MACOSX_DEPLOYMENT_TARGET=#{MacOS.version}",
+              "--qml-plugindir=#{pkgshare}/plugins",
+              "--verbose"]
+      args << "--debug" if build.with? "debug"
+
+      system python, "configure.py", *args
+      system "make"
+      system "make", "install"
+      system "make", "clean"
+    end
+    doc.install "doc/html", "examples" if build.with? "docs"
+  end
+
+  test do
+    system "#{bin}/pyuic5", "--version"
+    system "#{bin}/pylupdate5", "-version"
+    Language::Python.each_python(build) do |python, _version|
+      system python, "-c", "import PyQt5"
+      %w[
+        Gui
+        Location
+        Multimedia
+        Network
+        Quick
+        Svg
+        WebEngineWidgets
+        Widgets
+        Xml
+      ].each { |mod| system python, "-c", "import PyQt5.Qt#{mod}" }
+    end
+  end
+end
